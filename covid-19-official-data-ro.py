edeed1334c600bf6159dc42db93435474eb3b187
Gabriel Preda <gabi.preda@gmail.com>
2020-04-28 13:28:00 +0300

    ro covid 19 - 28/4/20 - daily update + refactor for url composition

--- covid-19-official-data-ro.py
+++ covid-19-official-data-ro.py
@@ -4,35 +4,6 @@ import os
 from content_parser.html_page_parser import HTMLPageParser
 from content_parser.regex_parser import get_item
 
-url_base = "https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-"
-url_data =\
-    [('2020-04-02',"2-aprilie-ora-13-00/"),
-     ('2020-04-03',"3-aprilie-2020-ora-13-00/"),
-     ('2020-04-04',"4-aprilie-2020-ora-13-00/"),
-     ('2020-04-05',"5-aprilie-2020-ora-13-00/"),
-     ('2020-04-06',"6-aprilie-2020-ora-13-00/"),
-     ('2020-04-07', "6-aprilie-2020-ora-13-00-2/"),
-     ('2020-04-08', "8-aprilie-2020-ora-13-00/"),
-     ('2020-04-09', "9-aprilie-2020-ora-13-00/"),
-     ('2020-04-10', "10-aprilie-2020-ora-13-00/"),
-     ('2020-04-11', "11-aprilie-2020-ora-13-00/"),
-     ('2020-04-12', "12-aprilie-2020-ora-13-00/"),
-     ('2020-04-13', "13-aprilie-2020-ora-13-00/"),
-     ('2020-04-14', "14-aprilie-2020-ora-13-00/"),
-     ('2020-04-15', "15-aprilie-2020-ora-13-00/"),
-     ('2020-04-16', "16-aprilie-2020-ora-13-00/"),
-     ('2020-04-17', "16-aprilie-2020-ora-13-00-2/"),
-     ('2020-04-18', "18-aprilie-2020-ora-13-00/"),
-     ('2020-04-19', "19-aprilie-2020-ora-13-00/"), 
-     ('2020-04-20', "20-aprilie-2020-ora-13-00/"),
-     ('2020-04-21', "21-aprilie-2020-ora-13-00/"),
-     ('2020-04-22', "22-aprilie-2020-ora-13-00/"),
-     ('2020-04-23', "23-aprilie-2020-ora-13-00/"),
-     ('2020-04-24', "24-aprilie-2020-ora-13-00/"),
-     ('2020-04-25', "25-aprilie-2020-ora-13-00/"),
-     ('2020-04-26', "26-aprilie-2020-ora-13-00/"),
-     ('2020-04-27', "27-aprilie-2020-ora-13-00/")
-    ]
 
 def get_ati_patients(paragraphs):
     return get_item(paragraphs, "La ATI, în acest moment, sunt internați+(.[0-9]*)")
@@ -46,14 +17,35 @@ def get_isolation(paragraphs):
 def get_tests(paragraphs):
     return get_item(paragraphs, "Până la această dată, la nivel național, au fost prelucrate+(.[ 0-9.]*)")
 
+def compose_current_date_url(crt_day):
+    url_base = "https://www.mai.gov.ro/informare-covid-19-grupul-de-comunicare-strategica-"
+    months = ['ianuarie', 'februarie', 'martie', 'aprilie', 'mai', 'iunie',
+          'iulie', 'august', 'septembrie', 'octombrie', 'noiembrie', 'decembrie']
+
+    current_date = '%d-%02d-%02d' % (crt_day.year, crt_day.month, crt_day.day)
+    current_url = '%d-%s-%d-ora-13-00/' % (crt_day.day, months[crt_day.month - 1], crt_day.year)
+
+    # exception treatment
+    if(current_date == "2020-04-02"):
+               current_url = '%d-%s-ora-13-00/' % (crt_day.day, months[crt_day.month  - 1])
+    if(current_date in ["2020-04-07", "2020-04-17"]):
+           current_url = '%d-%s-%d-ora-13-00-2/' % (crt_day.day - 1, months[crt_day.month - 1], crt_day.year)
+    
+    composed_url = f"{url_base}{current_url}"
+    return current_date, composed_url
 
 def parse_content():        
     hp = HTMLPageParser()
     all_data_df = pd.DataFrame()
     country_data_df = pd.DataFrame()
-    for current_date, current_url in url_data:
-        compose_url = f"{url_base}{current_url}"
-        paragraphs, tables = hp.parse_url(compose_url)
+
+    crt_day = dt.datetime.strptime("2020-04-02", "%Y-%m-%d")
+    end_day = dt.datetime.now()
+    delta = dt.timedelta(days=1)
+    while crt_day <= end_day:
+        crt_day += delta
+        current_date, composed_url = compose_current_date_url(crt_day)
+        paragraphs, tables = hp.parse_url(composed_url)
 
         # Process table data - to extract county-level data
         # retain only the first table
@@ -78,14 +70,23 @@ def parse_content():
     return all_data_df, country_data_df
 
 
-def check_number_of_counties(all_data_df):    
+def check_number_of_counties(all_data_df):
+    """
+    check that the number of counties in Romania is 42
+    """
     COUNTY_NUMBER = 42
     try:
         assert (len(all_data_df.County.unique())== (COUNTY_NUMBER + 1)), "wrong County number"
+        print("County number valid")
     except Exception as ex:
         print(f"Error: {ex}")
 
 def fix_decimal(data):
+    """
+    @param data - the data to have removed the decimal point
+    Data is as well converted to int
+    @return the cleaned int data
+    """
     data = str(data)
     data = data.replace(".", "")
     try:
@@ -98,24 +99,17 @@ def fix_decimal(data):
         
 def data_cleaning(all_data_df, country_data_df):
     
+    # replace '-' with not identified County data
     all_data_df.loc[all_data_df['County']=='–', 'County'] = 'Not identified'    
 
-    # replace - in Confirmed
+    # replace decimal point in Confirmed
     all_data_df.loc[all_data_df['Confirmed']=='–', 'Confirmed'] = 0
     # fix decimal point
     all_data_df['Confirmed'] = all_data_df['Confirmed'].apply(lambda x: fix_decimal(x))
-    
-    #all_data_df['Confirmed'] = all_data_df['Confirmed'].astype(str)
-    #all_data_df['Confirmed'] = all_data_df['Confirmed'].apply(lambda x: x.replace(".", ""))
-    #all_data_df['Confirmed'] = all_data_df['Confirmed'].astype(int)
 
-    # fix decimal point
+    # fix decimal point for multiple data
     country_data_df.loc[country_data_df['quarantine']==' ', 'quarantine'] = 0
     for feature in ['ati', 'quarantine', 'isolation', 'tests']:
-        
-        #country_data_df[feature] = country_data_df[feature].astype(str)
-        #country_data_df[feature] = country_data_df[feature].apply(lambda x: x.replace(".", ""))
-        #country_data_df[feature] = country_data_df[feature].astype(int)
         country_data_df[feature] = country_data_df[feature].apply(lambda x: fix_decimal(x))
         
     return all_data_df, country_data_df
@@ -130,10 +124,11 @@ def save_data(all_data_df, country_data_df):
     country_data_df.to_csv(os.path.join('ro_covid_19_time_series', "ro_covid_19_country_data_time_series.csv"), index=False)
 
 def check_data(all_data_df):    
-    all_data_df.groupby('Date')['Confirmed'].sum()
-    
-    
-def main():
+    print(f"All data cases: {all_data_df.groupby('Date')['Confirmed'].sum()}")
+
+import datetime as dt
+          
+def main():        
     all_data_df, country_data_df = parse_content()
     check_number_of_counties(all_data_df)
     all_data_df, country_data_df = data_cleaning(all_data_df, country_data_df)
